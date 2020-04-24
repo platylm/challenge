762cf9234b595369fc2ad38cfb0586844c30f1ea
platylm <n_nareenart@kkumail.com>
2020-04-24 09:55:07 +0700

    [Edited]:ปรับชื่อตัวแปรจาก i, j ให้เป็นชื่อที่ละเอียดกว่านี้ i = oneSideSock, j = pairSock

--- merchant/sock_merchant.go
+++ merchant/sock_merchant.go
@@ -2,13 +2,13 @@ package merchant
 
 func sockMerchant(number int, arrayNumber []int) int {
 	var matching int
-	for i, n := range arrayNumber {
-		for j := i + 1; j < len(arrayNumber); j++ {
-			if n == arrayNumber[j] {
-				if arrayNumber[i] != 0 {
+	for oneSideSock, n := range arrayNumber {
+		for pairSock := oneSideSock + 1; pairSock < len(arrayNumber); pairSock++ {
+			if n == arrayNumber[pairSock] {
+				if arrayNumber[oneSideSock] != 0 {
 					matching++
-					arrayNumber[i] = 0
-					arrayNumber[j] = 0
+					arrayNumber[oneSideSock] = 0
+					arrayNumber[pairSock] = 0
 				}
 			}
 		}
